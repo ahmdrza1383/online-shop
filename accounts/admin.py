73f829c6cb11c815a15d9a93a7ae3242dfb7634d
ahmadreza <ahmadreza.musavi@email.kntu.ac.ir>
2024-09-03 17:47:55 +0330

    add pages app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
-from accounts.models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('username', 'email')
+
+    list_display = UserAdmin.list_display  # + ('age', 'phone_number')
+    # fieldsets = UserAdmin.fieldsets + (
+    #     (None, {'fields': ('age',)}),
+    #     (None, {'fields': ('phone_number',)}),
+    # )
+    # add_fieldsets = UserAdmin.add_fieldsets + (
+    #     (None, {'fields': ('age',)}),
+    #     (None, {'fields': ('phone_number',)}),
+    # )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
